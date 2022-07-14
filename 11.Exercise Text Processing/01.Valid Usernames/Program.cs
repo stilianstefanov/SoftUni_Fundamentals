e883588026cbcaf02bd780a90b9f3d734af87e59
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-07-14 18:39:20 +0300

    Initial commit

---
+++ 11.Exercise Text Processing/01.Valid Usernames/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01.Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] passwords = Console.ReadLine().Split(", ");
+
+            foreach (var password in passwords)
+            {
+                if (password.Length > 3 && password.Length < 16)
+                {
+                    bool isValid = true;
+                    foreach(char ch in password)
+                    {
+                        if (!char.IsLetterOrDigit(ch) 
+                            && ch != '-'
+                            && ch != '_')
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        Console.WriteLine(password);
+                    }
+                }
+            }
+        }
+    }
+}
