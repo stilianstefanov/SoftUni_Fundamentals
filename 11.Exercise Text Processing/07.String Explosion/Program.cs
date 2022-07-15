ad074cdf878f329c653e6780b713ea3d57fc0f61
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-07-15 11:23:12 +0300

    Initial commit

---
+++ 11.Exercise Text Processing/07.String Explosion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace _07.String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            int power = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (power > 0 && inputString[i] != '>')
+                {
+                    inputString = inputString.Remove(i, 1);
+                    power--;
+                    i--;
+                }
+                else if (inputString[i] == '>')
+                {
+                    power += int.Parse(inputString[i + 1].ToString());
+                }
+            }
+            Console.WriteLine(inputString);
+        }
+    }
+}
