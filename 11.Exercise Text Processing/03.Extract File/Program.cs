0d30c813f5fe061ea073fc7fee1779b034b03cd4
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-07-15 09:18:14 +0300

    Initial commit

---
+++ 11.Exercise Text Processing/03.Extract File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] path = Console.ReadLine().Split("\\");
+
+            string[] fileAndExt = path[path.Length - 1].Split('.');
+
+            string file = fileAndExt[0];
+            string ext = fileAndExt[1];
+
+            Console.WriteLine($"File name: {file}");
+            Console.WriteLine($"File extension: {ext}");
+        }
+    }
+}
