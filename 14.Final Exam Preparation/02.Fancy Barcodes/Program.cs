0b945c983797474089cb25ad8a4d593fc9320dc2
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-07-25 17:43:28 +0300

    Initial commit

---
+++ 14.Final Exam Preparation/02.Fancy Barcodes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputCnt = int.Parse(Console.ReadLine());
+            string pattern = @"@[#]+([A-Z][A-Za-z\d]{4,}[A-Z])@[#]+";
+
+            for (int i = 0; i < inputCnt; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (Regex.IsMatch(input, pattern))
+                {
+                    string digitPattern = @"\d";
+                    MatchCollection digits = Regex.Matches(input, digitPattern);
+
+                    if (digits.Count > 0)
+                    {
+                        string productGroup = string.Join("", digits);
+
+                        Console.WriteLine($"Product group: {productGroup}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }
+                   
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
