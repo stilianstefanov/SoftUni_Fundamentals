121aaab098b81a971204ca9ff0497395fe548fea
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-07-26 11:43:22 +0300

    Initial commit

---
+++ 14.Final Exam Preparation/01.The Imitation Game/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01.The_Imitation_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] command = Console.ReadLine().Split("|");
+            while (command[0] != "Decode")
+            {
+                switch (command[0])
+                {
+                    case "Move":
+                        int numberOfLetters = int.Parse(command[1]);
+                        string substringToMove = input.Substring(0, numberOfLetters);
+                        input += substringToMove;
+                        input = input.Remove(0, numberOfLetters);
+                        break;
+                    case "Insert":
+                        int atIndex = int.Parse(command[1]);
+                        string valueToInsert = command[2];
+                        input = input.Insert(atIndex, valueToInsert);
+                        break;
+                    case "ChangeAll":
+                        string substringToReplace = command[1];
+                        string newValue = command[2];
+                        input = input.Replace(substringToReplace, newValue);
+                        break;
+                }
+                command = Console.ReadLine().Split("|");
+            }
+            Console.WriteLine($"The decrypted message is: {input}");
+        }
+    }
+}
---
+++ 14.Final Exam Preparation/02.Ad Astra/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.Ad_Astra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<sep>#|\|)(?<name>[A-Za-z\s]+)(\k<sep>)(?<date>\d{2}\/\d{2}\/\d{2})(\k<sep>)(?<calories>\d+)(\k<sep>)";
+
+            MatchCollection productMatches = Regex.Matches(input, pattern);
+
+            foreach (Match product in productMatches)
+            {
+                string name = product.Groups["name"].Value;
+                string expDate = product.Groups["date"].Value;
+                int calories = int.Parse(product.Groups["calories"].Value);
+
+                var newProduct = new Product(name, expDate, calories);
+                products.Add(newProduct);
+            }
+
+            int totalCalories = products
+                .Select(product => product.Calories)
+                .Sum();
+            int daysWithFood = totalCalories / 2000;
+
+            Console.WriteLine($"You have food to last you for: {daysWithFood} days!");
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Item: {product.Name}, Best before: {product.ExpDate}, Nutrition: {product.Calories}");
+            }
+        }
+    }
+
+    class Product
+    {
+        public Product(string name, string expDate, int calories)
+        {
+            Name = name;
+            ExpDate = expDate;
+            Calories = calories;
+        }
+
+        public string Name { get; set; }
+        public string ExpDate { get; set; }
+        public int Calories { get; set; }
+    }
+}
