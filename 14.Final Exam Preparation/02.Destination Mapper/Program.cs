22a191d16fe50a1f4c9449628909c7ff06660005
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-07-27 11:32:14 +0300

    Initial commit

---
+++ 14.Final Exam Preparation/02.Destination Mapper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<sep>[\/=])(?<name>[A-Z][A-Za-z]{2,})(\k<sep>)";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            int travelPoints = 0;
+
+            foreach (Match match in matches)
+            {
+                travelPoints += match.Groups["name"].Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", matches.Select(match => match.Groups["name"]))}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
