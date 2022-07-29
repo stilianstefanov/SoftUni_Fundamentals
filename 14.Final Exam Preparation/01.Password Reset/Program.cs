b30148af5c9b38844da572f28ab7b8512a227f31
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2022-07-29 10:34:43 +0300

    Initial commit

---
+++ 14.Final Exam Preparation/01.Password Reset/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace _01.Password_Reset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            string[] command = Console.ReadLine().Split();
+            while (command[0] != "Done")
+            {
+                switch (command[0])
+                {
+                    case "TakeOdd":
+                        input = TakeOdd(input);
+                        Console.WriteLine(input);
+                        break;
+                    case "Cut":
+                        int startIndex = int.Parse(command[1]);
+                        int length = int.Parse(command[2]);
+
+                        input = input.Remove(startIndex, length);
+                        Console.WriteLine(input);
+                        break;
+                    case "Substitute":
+                        if (input.Contains(command[1]))
+                        {
+                            input = input.Replace(command[1], command[2]);
+                            Console.WriteLine(input);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nothing to replace!");
+                        }
+                        break;
+                }
+                command = Console.ReadLine().Split();
+            }
+            Console.WriteLine($"Your password is: {input}");
+        }
+
+        private static string TakeOdd(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sb.Append(input[i]);
+                }
+            }            
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 14.Final Exam Preparation/03.Pirates/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Pirates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Town> townList = new List<Town>();
+
+            string[] townInfo = Console.ReadLine().Split("||");
+            while (townInfo[0] != "Sail")
+            {
+                string currTownName = townInfo[0];
+                int currPopulation = int.Parse(townInfo[1]);
+                int currGold = int.Parse(townInfo[2]);
+
+                if (!townList.Any(town => town.Name == currTownName))
+                {
+                    var newTown = new Town(currTownName);
+                    townList.Add(newTown);
+                }
+
+                var currTown = townList.Find(town => town.Name == currTownName);
+                currTown.Population += currPopulation;
+                currTown.Gold += currGold;
+
+                townInfo = Console.ReadLine().Split("||");
+            }
+
+            string[] command = Console.ReadLine().Split("=>");
+            while (command[0] != "End")
+            {
+                switch (command[0])
+                {
+                    case "Plunder":
+                        Plunder(townList, command);
+                        break;
+                    case "Prosper":
+                        Prosper(townList, command);
+                        break;
+                }
+                command = Console.ReadLine().Split("=>");
+            }
+
+            if (townList.Count == 0)
+            {
+                Console.WriteLine("Ahoy, Captain! All targets have been plundered and destroyed!");
+            }
+            else
+            {
+                Console.WriteLine($"Ahoy, Captain! There are {townList.Count} wealthy settlements to go to:");
+                Console.WriteLine(string.Join(Environment.NewLine, townList));
+            }
+        }
+
+        private static void Prosper(List<Town> townList, string[] command)
+        {
+            string currTownName = command[1];
+            int goldToIncrease = int.Parse(command[2]);
+
+            if (goldToIncrease < 0)
+            {
+                Console.WriteLine("Gold added cannot be a negative number!");
+                return;
+            }
+
+            var currTown = townList.Find(town => town.Name == currTownName);
+            currTown.Gold += goldToIncrease;
+
+            Console.WriteLine($"{goldToIncrease} gold added to the city treasury. {currTownName} now has {currTown.Gold} gold.");
+        }
+
+        private static void Plunder(List<Town> townList, string[] command)
+        {
+            string currTownName = command[1];
+            int populationToDecrease = int.Parse(command[2]);
+            int goldToDecrease = int.Parse(command[3]);
+
+            var currTown = townList.Find(town => town.Name == currTownName);
+            currTown.Population -= populationToDecrease;
+            currTown.Gold -= goldToDecrease;
+
+            Console.WriteLine($"{currTownName} plundered! {goldToDecrease} gold stolen, {populationToDecrease} citizens killed.");
+
+            if (currTown.Population == 0 || currTown.Gold == 0)
+            {
+                townList.Remove(currTown);
+                Console.WriteLine($"{currTownName} has been wiped off the map!");
+            }
+
+            return;
+        }
+    }
+
+    class Town
+    {
+        public Town(string name)
+        {
+            Name = name;            
+        }
+
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public int Gold { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} -> Population: {Population} citizens, Gold: {Gold} kg";
+        }
+    }
+}
